6b0076497254ccc6a40d8509d1ad9d9a6e6c1db0
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-19 10:29:49 +0000

    [Finish] 4-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
   get "homes/top"
